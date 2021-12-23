aff1f97cc77a09c39b72cd29d58060162c5723f6
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-12-22 21:00:54 -0500

    Update imports from env files

--- src/lib/apiUtilities/authorization.ts
+++ src/lib/apiUtilities/authorization.ts
@@ -1,8 +1,8 @@
 import { browser } from "$app/env";
 
 const authorizationApiUrl = "https://accounts.spotify.com";
-const baseUrl = import.meta.env.BASE_URL;
-const clientId = import.meta.env.SPOTIFY_CLIENT_ID as string;
+const baseUrl = import.meta.env.VITE_BASE_URL;
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID as string;
 
 export async function spotifyLogin(scopes: string[]): Promise<void> {
     if (browser) {
