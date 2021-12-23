93f0cd10b084132b79f16a043f2e374bf24f90e9
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-12-23 16:21:10 -0500

    Create separate login page

---
+++ src/routes/login.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Nav from "$lib/Nav.svelte";
+    import { accessTokenStore, spotifyLogin } from "$lib/apiUtilities/authorization";
+    import { onMount } from "svelte";
+    import { get } from "svelte/store";
+
+    const necessaryScopes = [
+        "playlist-read-private",
+        "playlist-modify-private",
+        "playlist-modify-public",
+    ];
+
+    onMount(() => {
+        if (get(accessTokenStore)) {
+            window.location.assign("/");
+        }
+    });
+</script>
+
+<Nav />
+<h1>Connect to your spotify account</h1>
+<button on:click={() => spotifyLogin(necessaryScopes)}>Login with Spotify</button>
